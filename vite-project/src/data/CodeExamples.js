1ef5d0815554d3108e3a18e24e0ed7c91ad6ba33
nourelyakine-cs <nouraraar595@gmail.com>
2026-02-15 22:04:00 +0100

    Update Hero section and styling

---
+++ vite-project/src/data/CodeExamples.js
@@ -0,0 +1,67 @@
+export const codeExamples = {
+ "App.jsx" :`import { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Simple Counter</h1>
+      <p>Count: {count}</p>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+
+      <button onClick={() => setCount(count - 1)} style={{ marginLeft: "10px" }}>
+        Decrease
+      </button>
+    </div>
+  );
+}
+`  
+, 
+"Hero.jsx" :`import { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Simple Counter</h1>
+      <p>Count: {count}</p>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+
+      <button onClick={() => setCount(count - 1)} style={{ marginLeft: "10px" }}>
+        Decrease
+      </button>
+    </div>
+  );
+}
+` ,  
+"Navbar.jsx" :`import { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Simple Counter</h1>
+      <p>Count: {count}</p>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+
+      <button onClick={() => setCount(count - 1)} style={{ marginLeft: "10px" }}>
+        Decrease
+      </button>
+    </div>
+  );
+}
+`   
+
+}
