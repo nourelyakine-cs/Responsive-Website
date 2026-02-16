43c200011fd5a952c1f9fe1d54a31e4aa36287e7
nourelyakine-cs <nouraraar595@gmail.com>
2026-02-16 22:25:32 +0100

    updating hero section

--- vite-project/src/components/Hero.jsx
+++ vite-project/src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, Sparkles } from "lucide-react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { codeExamples } from "../data/CodeExamples";
 import { nightOwl } from "react-syntax-highlighter/dist/esm/styles/hljs";
@@ -23,63 +23,83 @@ export default function Hero() {
       ></div>
       <div className="absolute top-20 left-4 sm:left-10 w-48 sm:w-72 h-48 sm:h-72 bg-blue-500/10 rounded-full blur-3xl animate-pulse " />
       <div className="absolute bottom-20 right-4 sm:right-10 w-64 sm:w-96 h-64 sm:h-96 bg-cyan-500/10 rounded-full blur-3xl animate-pulse delay-1000 " />
-      <div className="relative order-2 w-full">
-        <div className=" relative  p-3 sm:p-4 bg-white/5 backdrop-blur-xl rounded-xl sm:rounded-2xl shadow-2xl border border-white/10  ">
-          <div className="bg-gradient-to-br from-gray-900/20 to-gray-800/20 backdrop-blur-sm rounded-lg overflow-hidden h-[280px] sm:w-[350px] lg:h-[450px] border border-white/5 ">
-            <div className="flex items-center justify-between px-3 sm:px-4 py-2 sm:py-3 bg-white/5 backdrop-blur-sm border-b border-white/10">
-              <div className="flex items-center space-x-2 ">
-                <div className="flex items-center space-x-1 sm:space-x-2">
-                  <div className="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-red-500   " />
-                  <div className="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-yellow-500   " />
-                  <div className="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-green-500" />
-                </div>
-                <span className="text-xs sm:text-sm text-gray-300">
-                  CodeFlow AI{" "}
-                </span>
-              </div>
-              <ChevronDown className="w-3 h-3 sm:w-4 sm:h-4 text-gray-400 " />
+      <div className="max-w-7-xl mx-auto text-center relative w-full  ">
+        <div className="max-w-7xl mx-auto flex flex-col lg:grid lg:grid-cols-2 gap-6 sm:gap-8 lg:gap-12 items-center relative     ">
+          <div className="">
+            <div className="inline-flex items-center space-x-2 px-3 sm:px-4 py-2 bg-blue-500/10  border border-blue-500/20 rounded-full mb-4 sm:mb-6 animate-in slide-in-from-bottom duration-700       ">
+              <Sparkles className="w-4 h-4 text-blue-400 " />
+              <span className="text-x5 sm:text-sm text-blue-300 ">
+                Introducing CodeFlow AI{" "}
+              </span>
             </div>
+            <h1 className="text-5xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold mb-4 sm:mb-6 animate-in slide-in-from-bottom duration-700 delay-100 leading-tight  
+            
+            ">
+              <span className="bg-gradient-to-r from-white via-blue-100 to-cyan-100 bg-clip-text text-transparent  block mb-1 sm:mb-2 "> Code Faster</span>
+              <span className="bg-gradient-to-r from-white via-blue-100 to-cyan-100 bg-clip-text text-transparent  block mb-1 sm:mb-2"> Build Better</span>
+              <span className="bg-gradient-to-r from-white via-blue-100 to-cyan-100 bg-clip-text text-transparent  block mb-1 sm:mb-2">With CodeFlow AI </span>
+            </h1>
 
-            <div className="  p-3 sm:p-4 relative h-full">
-              <div className="flex  space-x-1 sm:space-x-2 mb-3 sm:mb-4 overflow-x-auto  ">
-                <button
-                  onClick={() => setActiveTab("App.jsx")}
-                  className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border ${activeTab == "App.jsx" ? "bg-blue-500/30 text-white border-blue-400/20" : " bg-white/5 text-gray-300 border-white/10 hover:bg-white/10"} transition-all duration-200 whitespace-nowrap`}
-                >
-                  App.jsx
-                </button>
-                <button
-                  onClick={() => setActiveTab("Hero.jsx")}
-                  className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border ${activeTab == "Hero.jsx" ? "bg-blue-500/30 text-white border-blue-400/20" : " bg-white/5 text-gray-300 border-white/10 hover:bg-white/10"} transition-all duration-200 whitespace-nowrap`}
-                >
-                  Hero.jsx
-                </button>
-                <button
-                  onClick={() => setActiveTab("Navbar.jsx")}
-                  className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border ${activeTab == "Navbar.jsx" ? "bg-blue-500/30 text-white border-blue-400/20" : " bg-white/5 text-gray-300 border-white/10 hover:bg-white/10"} transition-all duration-200 whitespace-nowrap`}
-                >
-                  Navbar.jsx
-                </button>
-              </div>
-              <div className="relative overflow-hidden flex-grow ">
-                <SyntaxHighlighter
-                  language="javascript"
-                  style={nightOwl}
-                  customStyle={{
-                    margin: 0,
-                    borderRadius: "8px",
-                    fontSize: "11px",
-                    lineHeight: "1.4",
-                    height: "100%",
-                    border: "1px solid #3c3c3c",
-                  }}
-                >
-                  {codeExamples[activeTab]}
-                </SyntaxHighlighter>
+          </div>
+
+          <div className="relative order-2 w-full">
+            <div className=" relative  p-3 sm:p-4 bg-white/5 backdrop-blur-xl rounded-xl sm:rounded-2xl shadow-2xl border border-white/10  ">
+              <div className="bg-gradient-to-br from-gray-900/20 to-gray-800/20 backdrop-blur-sm rounded-lg overflow-hidden h-[280px] sm:w-[350px] lg:h-[450px] border border-white/5 ">
+                <div className="flex items-center justify-between px-3 sm:px-4 py-2 sm:py-3 bg-white/5 backdrop-blur-sm border-b border-white/10">
+                  <div className="flex items-center space-x-2 ">
+                    <div className="flex items-center space-x-1 sm:space-x-2">
+                      <div className="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-red-500   " />
+                      <div className="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-yellow-500   " />
+                      <div className="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-green-500" />
+                    </div>
+                    <span className="text-xs sm:text-sm text-gray-300">
+                      CodeFlow AI{" "}
+                    </span>
+                  </div>
+                  <ChevronDown className="w-3 h-3 sm:w-4 sm:h-4 text-gray-400 " />
+                </div>
+
+                <div className="  p-3 sm:p-4 relative h-full">
+                  <div className="flex  space-x-1 sm:space-x-2 mb-3 sm:mb-4 overflow-x-auto  ">
+                    <button
+                      onClick={() => setActiveTab("App.jsx")}
+                      className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border ${activeTab == "App.jsx" ? "bg-blue-500/30 text-white border-blue-400/20" : " bg-white/5 text-gray-300 border-white/10 hover:bg-white/10"} transition-all duration-200 whitespace-nowrap`}
+                    >
+                      App.jsx
+                    </button>
+                    <button
+                      onClick={() => setActiveTab("Hero.jsx")}
+                      className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border ${activeTab == "Hero.jsx" ? "bg-blue-500/30 text-white border-blue-400/20" : " bg-white/5 text-gray-300 border-white/10 hover:bg-white/10"} transition-all duration-200 whitespace-nowrap`}
+                    >
+                      Hero.jsx
+                    </button>
+                    <button
+                      onClick={() => setActiveTab("Navbar.jsx")}
+                      className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border ${activeTab == "Navbar.jsx" ? "bg-blue-500/30 text-white border-blue-400/20" : " bg-white/5 text-gray-300 border-white/10 hover:bg-white/10"} transition-all duration-200 whitespace-nowrap`}
+                    >
+                      Navbar.jsx
+                    </button>
+                  </div>
+                  <div className="relative overflow-hidden flex-grow ">
+                    <SyntaxHighlighter
+                      language="javascript"
+                      style={nightOwl}
+                      customStyle={{
+                        margin: 0,
+                        borderRadius: "8px",
+                        fontSize: "11px",
+                        lineHeight: "1.4",
+                        height: "100%",
+                        border: "1px solid #3c3c3c",
+                      }}
+                    >
+                      {codeExamples[activeTab]}
+                    </SyntaxHighlighter>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          
         </div>
       </div>
     </section>
